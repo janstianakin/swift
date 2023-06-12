9660f8f895253da74eb8cc73c47a801fa98f61bf
Anakin Williams <136148496+janstianakin@users.noreply.github.com>
2023-06-12 15:16:34 +0800

    Update Bool.swift

--- stdlib/public/core/Bool.swift
+++ stdlib/public/core/Bool.swift
@@ -1,16 +1,5 @@
-//===----------------------------------------------------------------------===//
+// BOOL in class
 //
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2018 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-// Bool Datatype and Supporting Operators
-//===----------------------------------------------------------------------===//
 
 /// A value type whose instances are either `true` or `false`.
 ///
